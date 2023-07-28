89c114bbb35ea40447db2b7d806fad0b365dac29
linxr <linxr@ChaoyongdeiMac.local>
2023-07-28 17:12:06 +0800

    20230728

---
+++ example_data/test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+## test run of mitoSplitter, all cells in the example_data are singlets
+
+sh ../mitoSplitter_pipeline.sh -i sc_mix.bam -b barcodes.list -o example_result2 -l bam_file.list -g barcodes_cluster.txt test.log 2>&1 &
